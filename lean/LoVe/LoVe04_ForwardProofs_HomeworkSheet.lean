e23d060a24a877daf89c95e89c122173ef6cd6d1
Moritz macbook <mo.roos@gmx.de>
2025-05-15 18:26:13 +0200

    add hw4

--- lean/LoVe/LoVe04_ForwardProofs_HomeworkSheet.lean
+++ lean/LoVe/LoVe04_ForwardProofs_HomeworkSheet.lean
@@ -38,14 +38,37 @@ Hint: There is an easy way. -/
 
 theorem about_Impl_term :
     ∀a b : Prop, ¬ a ∨ b → a → b :=
-  sorry
+    fun a ↦ (
+      fun b ↦ (
+        fun hnab ↦ (
+          fun ha ↦ (
+            Or.elim hnab
+              (fun na ↦ False.elim (na ha))
+              (fun hb ↦ hb)
+          )
+        )
+      )
+    )
 
 /- 1.2 (2 points). Prove the same theorem again, this time by providing a
 structured proof, with `fix`, `assume`, and `show`. -/
 
 theorem about_Impl_struct :
     ∀a b : Prop, ¬ a ∨ b → a → b :=
-  sorry
+      assume a: Prop
+      assume b: Prop
+      assume hnab: ¬a ∨ b
+      assume ha: a
+      show b from
+        Or.elim hnab
+        (
+          assume na: ¬a
+          show b from False.elim (na ha)
+        )
+        (
+          assume hb:b
+          show b from hb
+        )
 
 
 /- ## Question 2 (6 points): Connectives and Quantifiers
@@ -56,7 +79,32 @@ rules for `∀`, `∨`, and `↔`. -/
 
 theorem Or_comm_under_All {α : Type} (p q : α → Prop) :
     (∀x, p x ∨ q x) ↔ (∀x, q x ∨ p x) :=
-  sorry
+      Iff.intro
+        (
+          assume h: (∀x, p x ∨ q x)
+          assume x: α
+          Or.elim (h x)
+          (
+            assume hp: p x
+            show q x ∨ p x from Or.inr hp
+          )
+          (
+            assume hq: q x
+            show q x ∨ p x from Or.inl hq
+          )
+        )
+        (
+          assume h: (∀x, q x ∨ p x)
+          assume x : α
+          Or.elim (h x) (
+            assume hq: q x
+            show p x ∨ q x from Or.inr hq
+          )
+          (
+            assume hp: p x
+            show p x ∨ q x from Or.inl hp
+          )
+        )
 
 /- 2.2 (3 points). We have proved or stated three of the six possible
 implications between `ExcludedMiddle`, `Peirce`, and `DoubleNegation` in the
@@ -71,15 +119,18 @@ namespace BackwardProofs
 
 theorem Peirce_of_DN :
     DoubleNegation → Peirce :=
-  sorry
+  assume DN: DoubleNegation
+  show Peirce from Peirce_of_EM (SorryTheorems.EM_of_DN DN)
 
 theorem EM_of_Peirce :
     Peirce → ExcludedMiddle :=
-  sorry
+  assume p: Peirce
+  show ExcludedMiddle from SorryTheorems.EM_of_DN (DN_of_Peirce p)
 
 theorem dn_of_em :
     ExcludedMiddle → DoubleNegation :=
-  sorry
+  assume em: ExcludedMiddle
+  show DoubleNegation from DN_of_Peirce (Peirce_of_EM em)
 
 end BackwardProofs
 
