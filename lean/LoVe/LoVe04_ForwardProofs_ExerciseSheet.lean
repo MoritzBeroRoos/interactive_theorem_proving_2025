27efaeac99d1f1bdbb59fef1072434270dd048e5
Moritz macbook <mo.roos@gmx.de>
2025-05-15 18:03:34 +0200

    add ex4

--- lean/LoVe/LoVe04_ForwardProofs_ExerciseSheet.lean
+++ lean/LoVe/LoVe04_ForwardProofs_ExerciseSheet.lean
@@ -19,48 +19,101 @@ namespace LoVe
 
 theorem I (a : Prop) :
     a → a :=
-  sorry
+  assume ha: a
+  show a from ha
 
 theorem K (a b : Prop) :
     a → b → b :=
-  sorry
+  assume ha: a
+  assume hb: b
+  show b from hb
+
+
 
 theorem C (a b c : Prop) :
     (a → b → c) → b → a → c :=
-  sorry
+  assume habc : (a → b → c)
+  assume hb: b
+  assume ha: a
+  show c from habc ha hb
 
 theorem proj_fst (a : Prop) :
     a → a → a :=
-  sorry
+  assume ha:a
+  assume ha2:a
+  show a from ha
 
 /- Please give a different answer than for `proj_fst`. -/
 
 theorem proj_snd (a : Prop) :
     a → a → a :=
-  sorry
+  assume ha: a
+  assume ha2: a
+  show a from ha2
 
 theorem some_nonsense (a b c : Prop) :
     (a → b → c) → a → (a → c) → b → c :=
-  sorry
+  assume habc: a → b → c
+  assume ha: a
+  assume hac: a → c
+  assume hb: b
+  show c from hac ha
 
 /- 1.2. Supply a structured proof of the contraposition rule. -/
 
 theorem contrapositive (a b : Prop) :
     (a → b) → ¬ b → ¬ a :=
-  sorry
+  assume hab: a → b
+  assume nb: ¬b
+  assume ha: a
+  show False from
+    nb (hab ha)
 
 /- 1.3. Supply a structured proof of the distributivity of `∀` over `∧`. -/
 
 theorem forall_and {α : Type} (p q : α → Prop) :
     (∀x, p x ∧ q x) ↔ (∀x, p x) ∧ (∀x, q x) :=
-  sorry
+  Iff.intro
+    (
+      assume h1: (∀ (x : α), p x ∧ q x)
+      show (∀ (x : α), p x) ∧ ∀ (x : α), q x from
+      And.intro
+      (
+        fix x: α
+        show p x from (h1 x).left
+      )
+      (
+        fix x: α
+        show q x from (h1 x).right
+      )
+    )
+    (
+      assume h1: (∀ (x : α), p x) ∧ ∀ (x : α), q x
+      show ∀ (x : α), p x ∧ q x from
+        assume x : α
+        show p x ∧ q x from
+          And.intro (h1.left x) (h1.right x)
+    )
 
 /- 1.4 (**optional**). Supply a structured proof of the following property,
 which can be used to pull a `∀` quantifier past an `∃` quantifier. -/
+#check Exists.elim
 
 theorem forall_exists_of_exists_forall {α : Type} (p : α → α → Prop) :
     (∃x, ∀y, p x y) → (∀y, ∃x, p x y) :=
-  sorry
+  assume he: (∃x, ∀y, p x y)
+  show (∀y, ∃x, p x y) from
+    assume  y: α
+
+    Exists.elim he (
+      assume a : α
+      show (∀ (y : α), p a y) → ∃ x, p x y from
+        assume pp: ∀(y : α), p a y
+        Exists.intro a (pp y)
+
+    )
+
+
 
 
 /- ## Question 2: Chain of Equalities
@@ -76,11 +129,14 @@ theorem forall_exists_of_exists_forall {α : Type} (p : α → α → Prop) :
 Hint: This is a difficult question. You might need the tactics `simp` and
 `ac_rfl` and some of the theorems `mul_add`, `add_mul`, `add_comm`, `add_assoc`,
 `mul_comm`, `mul_assoc`, , and `Nat.two_mul`. -/
-
+#check two_mul
 theorem binomial_square (a b : ℕ) :
     (a + b) * (a + b) = a * a + 2 * a * b + b * b :=
-  sorry
-
+  calc
+  (a + b) * (a + b) = a * (a + b) + b * (a + b) := by simp[add_mul]
+  _ = a * a + a * b + b * a + b * b := by simp[mul_add, add_assoc]
+  _ = a * a + a * b + a * b + b * b := by simp[mul_comm]
+  _ = a * a + 2 * a * b + b * b := by simp[Nat.two_mul, add_mul, add_assoc]
 /- 2.2 (**optional**). Prove the same argument again, this time as a structured
 proof, with `have` steps corresponding to the `calc` equations. Try to reuse as
 much of the above proof idea as possible, proceeding mechanically. -/
@@ -98,9 +154,20 @@ rule for `∀` is inconsistent, using a structured proof. -/
 axiom All.one_point_wrong {α : Type} (t : α) (P : α → Prop) :
     (∀x : α, x = t ∧ P x) ↔ P t
 
+def R : Nat → Prop :=
+  fun x ↦ x = 0
+
 theorem All.proof_of_False :
     False :=
-  sorry
+    have hR: R 0 := by rfl
+    have hall: (∀x : Nat, x = 0 ∧ R x) := Iff.mpr (All.one_point_wrong 0 R) hR
+    have incon: 1 = 0 := (hall 1).left
+    have bla: (1 = 0) → False := sorry
+    show False from bla incon
+
+
+
+
 
 /- 3.2 (**optional**). Prove that the following wrong formulation of the
 one-point rule for `∃` is inconsistent, using a structured proof. -/
