ba9b183176a762388619ebd5a8c166862bb05e9c
Moritz Roos macbook <mo.roos@gmx.de>
2025-06-28 22:26:35 +0200

    finish ex shett 10

--- lean/LoVe/LoVe10_HoareLogic_HomeworkSheet.lean
+++ lean/LoVe/LoVe10_HoareLogic_HomeworkSheet.lean
@@ -44,7 +44,18 @@ capture the fact that the variable `n` does not change. -/
 
 theorem FACT_correct (n₀ : ℕ) :
     {* fun s ↦ s "n" = n₀ *} (FACT) {* fun s ↦ s "r" = fact n₀ *} :=
-  sorry
+    show {* fun s ↦ s "n" = n₀ *}
+    (  Stmt.assign "i" (fun s ↦ 0);
+       Stmt.assign "r" (fun s ↦ 1);
+       Stmt.invWhileDo (fun s ↦ s "r" = fact (s "i") ∧ s "n" = n₀) (fun s ↦ s "i" ≠ s "n")
+      (Stmt.assign "i" (fun s ↦ s "i" + 1);
+       Stmt.assign "r" (fun s ↦ s "r" * s "i")))
+       {* fun s ↦ s "r" = fact n₀ *}
+    from by
+      vcg
+      all_goals aesop (add norm fact) <;> ac_rfl
+
+
 
 
 /- ## Question 2 (5 points + 1 bonus point):
@@ -72,33 +83,49 @@ namespace PartialHoare
 
 theorem consequence {P P' Q Q' S} (h : {* P *} (S) {* Q *})
       (hp : ∀s, P' s → P s) (hq : ∀s, Q s → Q' s) :
-    {* P' *} (S) {* Q' *} :=
-  sorry
+    {* P' *} (S) {* Q' *} := by
+    intro s t hP'
+    aesop
 
 theorem assign_intro {P x a} :
-    {* fun s ↦ P (s[x ↦ a s]) *} (Stmt.assign x a) {* P *} :=
-  sorry
+    {* fun s ↦ P (s[x ↦ a s]) *} (Stmt.assign x a) {* P *} := by
+    intro s t h
+    aesop
 
 theorem assert_intro {P Q : State → Prop} :
-    {* fun s ↦ Q s → P s *} (Stmt.assert Q) {* P *} :=
-  sorry
+    {* fun s ↦ Q s → P s *} (Stmt.assert Q) {* P *} := by
+    intro _ _ _ _
+    aesop
 
 theorem seq_intro {P Q R S T}
       (hS : {* P *} (S) {* Q *}) (hT : {* Q *} (T) {* R *}) :
-    {* P *} (Stmt.seq S T) {* R *} :=
-  sorry
+    {* P *} (Stmt.seq S T) {* R *} := by
+    intro s t Ps hST
+    aesop
 
 theorem choice_intro {P Q Ss}
       (h : ∀i (hi : i < List.length Ss), {* P *} (Ss[i]'hi) {* Q *}) :
-    {* P *} (Stmt.choice Ss) {* Q *} :=
-  sorry
+    {* P *} (Stmt.choice Ss) {* Q *} := by
+    intro s t Ps hChoice
+    aesop
 
 /- 2.2 (1 bonus point). Prove the rule for `loop`. Notice the similarity with
 the rule for `while` in the WHILE language. -/
 
 theorem loop_intro {P S} (h : {* P *} (S) {* P *}) :
-    {* P *} (Stmt.loop S) {* P *} :=
-  sorry
+    {* P *} (Stmt.loop S) {* P *} := by
+  intro s t Ps hloop
+  unfold PartialHoare at *
+  generalize eq_Ss: (S.loop, s) = Ss at hloop
+  induction hloop generalizing S s with
+  | loop0 S s' => aesop
+  | loop S' s' t u hSst hLoopStu hind hind2 =>
+    have : P t := by aesop
+    #check hind2 --why isn't it forall quantified over S here?? it shows like that in the infoview?
+    have : P u := by aesop
+    --exact this
+  | _ => aesop
+
 
 end PartialHoare
 
