df0cd27d5ab6bece0ae69f94f16e5970575c8030
Moritz Roos macbook <mo.roos@gmx.de>
2025-06-29 16:03:47 +0200

    add homeowork 9 b sheet

--- lean/LoVe/LoVe09_OperationalSemantics_HomeworkBSheet.lean
+++ lean/LoVe/LoVe09_OperationalSemantics_HomeworkBSheet.lean
@@ -45,10 +45,13 @@ concerned), and `repeat 0 S` has the same effect as `skip`.
 1.1 (2 points). Complete the following definition of a big-step semantics: -/
 
 inductive BigStep : Stmt × State → State → Prop
-  | skip (s) :
-    BigStep (Stmt.skip, s) s
-  | assign (x a s) : BigStep (Stmt.assign x a, s) (s[x ↦ a s])
-  | seq (S1 S2 s):  BigStep (Stmt.seq S1 S2, s) s
+  | skip {s} : BigStep (Stmt.skip, s) s
+  | assign {x a s} : BigStep (Stmt.assign x a, s) (s[x ↦ a s])
+  | seq {S1 S2 s s' s''} (hS1: BigStep (S1, s) s') (hS23: BigStep (S2,s') s''):  BigStep (Stmt.seq S1 S2, s) s''
+  | unlessDoTrue {S b s} (hb: b s) : BigStep (Stmt.unlessDo b S, s) s
+  | unlessDoFalse {S b s s'} (nhb: ¬(b s)) (hS: BigStep (S, s) s'): BigStep (Stmt.unlessDo b S, s) s'
+  | repeat0 {S s} : BigStep (Stmt.repeat 0 S, s) s
+  | repeat_succ_n {S s s' s'' n} (h1: BigStep (S,s) s') (hn: BigStep (Stmt.repeat n S, s') s'') : BigStep (Stmt.repeat (n+1) S, s) s''
 -- enter the missing cases here
 
 infix:110 " ⟹ " => BigStep
@@ -57,8 +60,13 @@ infix:110 " ⟹ " => BigStep
 semantics: -/
 
 inductive SmallStep : Stmt × State → Stmt × State → Prop
-  | assign (x a s) :
-    SmallStep (Stmt.assign x a, s) (Stmt.skip, s[x ↦ a s])
+  | assign {x a s} : SmallStep (Stmt.assign x a, s) (Stmt.skip, s[x ↦ a s])
+  | seq {S1 S2 S1' s s'} (hS1: SmallStep (S1,s) (S1', s')): SmallStep (S1; S2, s) (S1'; S2, s')
+  | seq_skip {S s} : SmallStep (Stmt.skip; S, s) (S,s)
+  | unlessDoTrue {b S s} (hb: (b s)): SmallStep (Stmt.unlessDo b S, s) (Stmt.skip, s)
+  | unlessDoFalse {b S s} (nhb: ¬(b s)) : SmallStep (Stmt.unlessDo b S, s) (S, s)
+  | repeat0 {S s}:  SmallStep (Stmt.repeat 0 S, s) (Stmt.skip, s)
+  | repeat_succ_n {n S s}: SmallStep (Stmt.repeat (n+1) S, s) (S;Stmt.repeat n S, s)
 -- enter the missing cases here
 
 infixr:100 " ⇒ " => SmallStep
@@ -75,8 +83,14 @@ Towards this goal, you are asked to define a __measure__ function: a function
 how "big" the statement is. The measure should be defined so that it strictly
 decreases with each small-step transition. -/
 
-def mess : Stmt → ℕ
-  | Stmt.skip         => 0
+def mess : Stmt → ℕ := fun
+  | .skip => 0
+  | .assign _ _ => 1
+  | .seq S1 S2 => mess S1 + mess S2 + 1
+  | .unlessDo _ S => mess S + 1
+  | .repeat 0 _ => 1
+  | .repeat (n+1) S => 2 + mess S + mess (Stmt.repeat n S) --+1 for the application of the repeat_succ rule, +1 for the sequence we build on top of the repeat-rest.
+
 -- enter the missing cases here
 
 /- You can validate your answer as follows. Consider the following program
@@ -121,8 +135,15 @@ def S₆ : Stmt :=
 transition. If necessary, revise your answer to question 1.3. -/
 
 theorem SmallStep_mess_decreases {Ss Tt : Stmt × State} (h : Ss ⇒ Tt) :
-    mess (Prod.fst Ss) > mess (Prod.fst Tt) :=
-  sorry
+    mess (Prod.fst Ss) > mess (Prod.fst Tt) := by
+  induction h with
+  | assign => simp [mess]
+  | seq hS1 ih => simp [mess, ih]
+  | seq_skip => simp [mess]
+  | unlessDoTrue hb => simp [mess]
+  | unlessDoFalse nhb => simp [mess]
+  | repeat0 => simp [mess]
+  | repeat_succ_n => simp [mess];linarith
 
 
 /- ## Question 2 (3 points): Inversion Rules
@@ -131,8 +152,9 @@ theorem SmallStep_mess_decreases {Ss Tt : Stmt × State} (h : Ss ⇒ Tt) :
 of `unless`. -/
 
 theorem BigStep_ite_iff {B S s t} :
-    (Stmt.unlessDo B S, s) ⟹ t ↔ (B s ∧ s = t) ∨ (¬ B s ∧ (S, s) ⟹ t) :=
-  sorry
+    (Stmt.unlessDo B S, s) ⟹ t ↔ (B s ∧ s = t) ∨ (¬ B s ∧ (S, s) ⟹ t) := by
+    cases em (B s)
+    <;> aesop (add norm BigStep.unlessDoTrue, norm BigStep.unlessDoFalse, safe cases BigStep)
 
 /- 2.2 (2 points). Prove the following inversion rule for the big-step
 semantics of `repeat`. -/
@@ -140,7 +162,11 @@ semantics of `repeat`. -/
 theorem BigStep_repeat_iff {n S s u} :
     (Stmt.repeat n S, s) ⟹ u ↔
     (n = 0 ∧ u = s)
-    ∨ (∃m t, n = m + 1 ∧ (S, s) ⟹ t ∧ (Stmt.repeat m S, t) ⟹ u) :=
-  sorry
+    ∨ (∃m t, n = m + 1 ∧ (S, s) ⟹ t ∧ (Stmt.repeat m S, t) ⟹ u) := by
+    apply Iff.intro
+    · intro repeatS
+      cases repeatS
+      <;> aesop
+    · aesop (add safe BigStep.repeat0, safe BigStep.repeat_succ_n, unsafe cases BigStep)
 
 end LoVe
