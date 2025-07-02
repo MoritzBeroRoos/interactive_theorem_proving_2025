95f48ed2afab6b26ce0c8ca8d6a749d54b1eab18
Moritz Roos macbook <mo.roos@gmx.de>
2025-07-02 19:07:45 +0200

    add ex sheet 12

--- lean/LoVe/LoVe12_LogicalFoundationsOfMathematics_ExerciseSheet.lean
+++ lean/LoVe/LoVe12_LogicalFoundationsOfMathematics_ExerciseSheet.lean
@@ -37,29 +37,64 @@ theorem List.length_add :
     ∀xs ys, List.length xs = List.length ys →
       List.length (List.add xs ys) = List.length xs
   | [], [] =>
-    sorry
+    by simp [add]
   | x :: xs, y :: ys =>
-    sorry
-  | [], y :: ys =>
-    sorry
-  | x :: xs, [] =>
-    sorry
+    by
+      simp [add, length_add xs ys]
+      apply length_add xs ys
+  | [], y :: ys => by
+    simp
+  | x :: xs, [] => by
+    simp
 
 /- 1.2. Define componentwise addition on vectors using `List.add` and
 `List.length_add`. -/
-
+#check Vector
 def Vector.add {n : ℕ} : Vector ℤ n → Vector ℤ n → Vector ℤ n :=
-  sorry
+ fun a b ↦ Subtype.mk (List.add a.val b.val) (by
+  have :  (a.val).length = n := by exact a.property
+  simp [←this] --wWTF warum nicht rw?
+  apply List.length_add
+  simp [a.property, b.property]
+  )
 
 /- 1.3. Show that `List.add` and `Vector.add` are commutative. -/
+@[simp]
+theorem List.add_left (xs: List ℤ):
+List.add [] xs = [] := by
+  cases xs
+  simp[add]
+  simp[add]
+
+@[simp]
+theorem List.add_right (xs: List ℤ):
+List.add xs [] = [] := by
+  cases xs
+  simp[add]
+  simp[add]
+
 
 theorem List.add.comm :
-    ∀xs ys, List.add xs ys = List.add ys xs :=
-  sorry
+    ∀xs ys, List.add xs ys = List.add ys xs := by
+    intro xs ys
+    induction xs with
+    | nil => simp
+    | cons head tail ih =>
+      cases ys with
+      | nil => simp
+      | cons head' tail' =>
+        simp [add]
+        apply And.intro
+        · ring
+        · apply List.add.comm
+
 
 theorem Vector.add.comm {n : ℕ} (u v : Vector ℤ n) :
-    Vector.add u v = Vector.add v u :=
-  sorry
+    Vector.add u v = Vector.add v u := by
+    apply Subtype.eq
+    unfold add --why is this step not needed?
+    apply List.add.comm
+
 
 
 /- ## Question 2: Integers as Quotients
@@ -74,17 +109,37 @@ Lean's predefined type `Int` (= `ℤ`): -/
 /- 2.1. Define negation on these integers. Observe that if `(p, n)` represents
 an integer, then `(n, p)` represents its negation. -/
 
+
+
+
 def Int.neg : Int → Int :=
-  sorry
+ Quotient.lift (fun np ↦ ⟦(np.2, np.1)⟧) ( by
+ intro np1 np2 h
+ simp
+ apply Quotient.sound
+ simp [Setoid_Iff] at *
+ linarith
+ )
+
 
 /- 2.2. Prove the following theorems about negation. -/
 
 theorem Int.neg_eq (p n : ℕ) :
     Int.neg ⟦(p, n)⟧ = ⟦(n, p)⟧ :=
-  sorry
+  by rfl
 
+
+#check Quotient.ind
 theorem int.neg_neg (a : Int) :
-    Int.neg (Int.neg a) = a :=
-  sorry
+    Int.neg (Int.neg a) = a := by
+    induction a using Quotient.ind
+    constructor--dafuq? this works?
+
+
+
+
+
+
+
 
 end LoVe
